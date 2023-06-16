282d97fa7b4b7f38fd7f15cf64c89712f8cc92ac
Lune200 <nekomoon2003@gmail.com>
2023-06-16 20:02:14 -0300

    final

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine ("Digite um numero para tabuada: ");
+
+
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int y = 0;
+int produto;
+while (y<=10)
+{
+   produto = x * y;
+   Console.WriteLine ($"{x} X {y}= {produto}");
+    y ++;
+}
